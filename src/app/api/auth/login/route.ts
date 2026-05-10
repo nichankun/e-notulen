059d2dcdce37e7e2b82afe7fc6251da2573e2062
Imam Hayatul Jihad <fcbimam@gmail.com>
2026-05-10 09:36:18 +0800

    fix

--- src/app/api/auth/login/route.ts
+++ src/app/api/auth/login/route.ts
@@ -90,7 +90,7 @@ export async function POST(request: Request) {
 
     // 5. Set HTTP-Only Cookie
     const cookieStore = await cookies();
-    const isProd = false;
+    const isProd = process.env.NODE_ENV === "production";
 
     cookieStore.set("auth_token", token, {
       httpOnly: true,
